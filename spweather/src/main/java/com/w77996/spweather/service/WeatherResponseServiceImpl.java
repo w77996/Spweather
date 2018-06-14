aeafd356d0d2dc857c2e8b7a4fb05060ae2a4931
w77996 <912513404@qq.com>
2018-06-14 23:07:19 +0800

    first commit

--- spweather/src/main/java/com/w77996/spweather/service/WeatherResponseServiceImpl.java
+++ spweather/src/main/java/com/w77996/spweather/service/WeatherResponseServiceImpl.java
@@ -1,9 +1,17 @@
 package com.w77996.spweather.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
 import com.w77996.spweather.vo.WeatherResponse;
 
 public class WeatherResponseServiceImpl implements WeatherDataService {
 
+	
+	private static final String WEATHER_URL = "http://wthrcdn.etouch.cn/weather_mini?";
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	@Override
 	public WeatherResponse getDataByCityId(String cityId) {
 		// TODO Auto-generated method stub
